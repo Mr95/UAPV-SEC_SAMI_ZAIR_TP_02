3ce9553e36bb49374af4f3d813b64b01ff35c67a
unknown <sami.zair@hotmail.com>
2020-12-30 19:28:03 +0100

    second commit get pix_list

--- main.py
+++ main.py
@@ -9,11 +9,20 @@ def base10ToByte(val):
     return zerosToAdd*"0"+res
 
 
+def getPixelList(path):
+    r=png.Reader(path)
+    pix_list = []
+    for row in r.asRGBA8()[2]:
+        pix_list.append(list(row))
+    return pix_list
 
 def main():
 
         stringToHide = input("enter string to hide : ")
         asciiBinary = [base10ToByte(ord(c)) for c in stringToHide]
         print('ASCII',asciiBinary)
+        file_path = input("Enter png path : ")
+        pix_list = getPixelList(file_path)
+        print('pixels',len(pix_list))
 
 main()
